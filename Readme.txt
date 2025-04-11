124995bfa90d347a9e46e285e03f02a60bcaab23
Gitesh Sharma <gitesh15796@gmail.com>
2025-04-11 12:57:19 +0530

    product detail added

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,9 @@
 BN Techpack landing page site
+
+
+1.) custom code (logic) -> (html/javascript/css/markup(.md files, product html)) -> git
+2.) custom data (media) (video(youtube)/images(pininterest)/icons/fonts/) -> cdn (cloudfront/s3/versel/cdnjs/etc..)
+3.) json data (database) -> (firebase store/air table) or fetch api or sdk to run query 
+
+
+MVC -> model(data)/view(html)/controller(logic)
--- assets/js/product-detail.js
+++ assets/js/product-detail.js
@@ -2,37 +2,79 @@
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get('id');
 
+// fetch('./data/productuctData.json').then(response => productData = response);
+
 // product data
 const productData = {
-  "BNELF-10-100-ML-1HP": {
-    name: "BNELF-10-100-ML-1HP",
-    image: "assets/img/products/fully-auto-liquid-filling-machine.png",
-    description: "A high-performance semi-auto liquid filling machine with 1HP power for 10-100ml filling range.",
-    power: "20W - 50W 230v AC 50Hz",
-    airPressure: "0.4 - 0.6MPa",
-    fillingSpeed: "5-25 -10 -50 bottles/min",
-    fillingAccuracy: "≤±1%" ,
-    machineDimensionLxWxH: "950 X320 X 340mm" ,
+   // viscosity for water to honey level
+  "BNTPALF: 10-100 ML -GM 1HP/ 2HP": {
+    name: "BNTPALF: 10-100 ML -GM 1HP/2HP",
+    image: "assets/img/products/semi-auto-liquid-filling-machine-water-to-honey-level.png",
+    power: "20W-50W 230 AC 50Hz",
+    airPressure: "4 to 6 Bar",
+    fillingSpeed: "5-10 - 10-20 bottles/min",
+    fillingAccuracy: "≤±1-2% ML-GM" ,
+    machineDimensionLxWxH: "950X320X340mm - 1230x470X350mm" ,
     weight: "20KG / 40 kg"  
   },
-  "BNELF-10-100-ML-2HP": {
-    name: "BNELF-10-100-ML-2HP",
-    image: "assets/img/products/fully-auto-liquid-filling-machine.png",
-    description: "An upgraded 2HP variant for faster and more efficient filling.",
-    power: "20W -50W 230v AC 50Hz",
-    airPressure: "0.4-0.6MPa",
-    fillingSpeed: "5-25 -10 -50 bottles/min",
-    fillingAccuracy: "≤±1%" ,
-    machineDimensionLxWxH: "950 X 320 X 340mm" ,
+  "BNTPALF:50-500 ML –GM 1HP/2HP": {
+    name: "BNTPALF: 50-500 ML –GM 1HP/2HP",
+    image: "assets/img/products/semi-auto-liquid-filling-machine-water-to-honey-level.png",
+    power: "20W-50W 230 AC 50Hz",
+    airPressure: "4 TO 6 Bar",
+    fillingSpeed: "5-10 -10-20 bottles/min",
+    fillingAccuracy: "≤±1-2% ML-GM" ,
+    machineDimensionLxWxH: "920 X 285 X275 mm - 920 X 485 X275 mm" ,
     weight: "20KG / 40kg"  
   },
-  // Add other products similarly
+  "BNTPALF:100-1000 ML –GM 1HP/ 2HP": {
+    name: "BNTPALF: 100-1000 ML –GM 1HP/2HP",
+    image: "assets/img/products/semi-auto-liquid-filling-machine-water-to-honey-level.png",
+    power: "20W -50W 230 AC 50Hz",
+    airPressure: "4 TO 6 Bar",
+    fillingSpeed: "5-10 -10 -20 bottles/min",
+    fillingAccuracy: "≤±1-2% ML-GM" ,
+    machineDimensionLxWxH: "1320 X 285 X275 mm - 1320 x 485 X 275 mm" ,
+    weight: "20KG / 40kg"  
+  },
+  "BNTPALF:500-5000 ML –GM 1HP/2HP": {
+    name: "BNTPALF: 500-5000 ML –GM 1HP/2HP",
+    image: "assets/img/products/semi-auto-liquid-filling-machine-water-to-honey-level.png",
+    power: "20W -50W 230 AC 50Hz",
+    airPressure: "4 TO 6 Bar",
+    fillingSpeed: "5-10 -10 -20 bottles/min",
+    fillingAccuracy: "≤±1-2% ML-GM" ,
+    machineDimensionLxWxH: "950 X 320 X 340mm - 1230 x 470 X 350mm" ,
+    weight: "20KG / 40kg"  
+  },
+
+  // viscosity for water to edible oil level
+  "BNE4000-MLT-1HE": {
+    name: "BNE4000-MLT-1HE",
+    image: "assets/img/products/semi-auto-liquid-filling-machine-water-to-edible-oil-level.png",
+    power: "-",
+    airPressure: "4 TO 6 Bar",
+    fillingSpeed: "5-10 bottles/min",
+    fillingAccuracy: "≤±1-2% ML" ,
+    machineDimensionLxWxH: "-" ,
+    weight: "5 kg"  
+  },
+  "BNE4000-MLT-2HEP": {
+    name: "BNE4000-MLT-2HE",
+    image: "assets/img/products/semi-auto-liquid-filling-machine-water-to-edible-oil-level.png",
+    power: "-",
+    airPressure: "4 TO 6 Bar",
+    fillingSpeed: "5-10 bottles/min",
+    fillingAccuracy: "≤±1-2% ML" ,
+    machineDimensionLxWxH: "-" ,
+    weight: "12 kg"  
+  },
+
 };
 
 // 3. Get DOM elements
 const nameElement = document.getElementById("product-model-name");
 const imageElement = document.getElementById("product-model-image");
-const descElement = document.getElementById("product-model-description");
 const powerElement = document.getElementById("product-model-power");
 const airPressureElement = document.getElementById("product-model-air-pressure");
 const fillingSpeedElement = document.getElementById("product-model-filling-speed");
@@ -45,7 +87,6 @@ if (productId && productData[productId]) {
   const product = productData[productId];
   nameElement.textContent = product.name;
   imageElement.src = product.image;
-  descElement.textContent = product.description;
   powerElement.textContent = product.power;
   airPressureElement.textContent = product.airPressure;
   fillingSpeedElement.textContent = product.fillingSpeed;
